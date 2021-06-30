b46eaa50de9d35466b47ac358cbc44061ffd7e85
agre6595 <aaron.greenville@sydney.edu.au>
2021-06-30 21:19:01 +1000

    Changed quantiles to percentiles in 20% increments. Added Moran's I test

--- scripts/endemism-fire-overlap.R
+++ scripts/endemism-fire-overlap.R
@@ -1,5 +1,6 @@
 ##****************************************************************************##
 # fire severity and weighted endemism overlap for invertebrates
+# - point data
 # Aaron Greenville
 # June 2020
 ##****************************************************************************##
@@ -13,11 +14,13 @@ library(tmap)
 #library(ggplot2)
 
 ## >> load endemism results (polygons) ####
-
-load("data/m1_point_1.rds")
-
-load("data/m1_point_0-5.rds")
 # load("data/m1_poly_0-5.rds")
+m1 <- m1.poly
+
+## >> load endemism results (points) ####
+#load("data/m1_point_1.rds")
+load("data/m1_point_0-5.rds")
+
 
 aust <- readOGR("C:\\Users\\aaron\\Dropbox (Sydney Uni)\\Projects\\GIS_files\\World boundaries\\Aust\\AustraliaAdmin.shp")
 plot(aust)
@@ -47,8 +50,15 @@ plot(aust.utm, add = TRUE)
 
 ## >> break endemism value into quantiles and assign to new field ####
 
-m1$corrected_endemism_quantile <- cut(m1$corrected_endemism , breaks=quantile(m1$corrected_endemism),
-                                         labels=1:4, include.lowest=TRUE)
+# m1$corrected_endemism_quantile <- cut(m1$corrected_endemism , breaks=quantile(m1$corrected_endemism),
+#                                          labels=1:4, include.lowest=TRUE)
+
+# break into percentiles
+m1$corrected_endemism_quantile <- cut(m1$corrected_endemism , breaks=quantile(m1$corrected_endemism,
+                                                                              probs = seq(0, 1, by = 0.20)),
+                                      labels=c("0-20%","20-40%", "40-60%", "60-80%", "80-100%"), 
+                                      include.lowest=TRUE)
+
 
 # check
 Corrected.weighted.endemism.q <- tm_shape(m1) +
@@ -64,92 +74,82 @@ Corrected.weighted.endemism.q <- tm_shape(m1) +
               lwd = 2, border.col = 1)
 
 
-## >> Dissolve polys based on corrected_endemism_quantile ####
-m1.dissolve <- aggregate(m1, by = "corrected_endemism_quantile")
-
-  
-# check
-  tm_shape(m1.dissolve) +
-    tm_polygons("corrected_endemism_quantile", 
-                #style="pretty", 
-                title="Points: Corrected weighted endemism",
-                lwd = 0.5,
-                palette="OrRd")+ #"-RdBu"
-    tm_shape(aust)+
-    tm_polygons("PLACENAME", 
-                alpha = 0,
-                legend.show = F,
-                lwd = 2, border.col = 1)
-  
 
 ## >> Extract fire severity values in each endemism polygon ####
 
 plot(fire_severity)  
 
 # convert to same coordinate system  
-m1.dissolve.utm <- spTransform(m1.dissolve,
-                            crs(fire_severity))
-
 m1.utm <- spTransform(m1,crs(fire_severity))
 
 # crop to mainland extent
 fire.sev.crop <- crop(fire_severity, (analysis.area.utm))  
 m1.utm.crop <- crop(m1.utm, (analysis.area.utm))
 
+# replace fire severity class 5 (Very high) with 4 (high) for mapping below
+fire.sev.crop.recl <- reclassify(fire.sev.crop, c(4,5,4))
 
-# extract fire severity for each endemism polygon  
-extract.fire <- exactextractr::exact_extract(fire.sev.crop, m1.utm.crop, include_area = TRUE) 
-
-# add endemism col to list elements
-extract.fire.all<- Map(cbind,extract.fire, Endemism = c(1:4) )
+# extract fire severity for each endemism polygon. Note still has all classes for flexibility  
+extract.fire <- exactextractr::exact_extract(fire.sev.crop, m1.utm.crop, include_area = TRUE,
+                                             include_cols  = "corrected_endemism_quantile") 
 
 # collapse list in df  
-
-extract.fire.all.df<-do.call("rbind",extract.fire.all )
+extract.fire.all.df<-do.call("rbind",extract.fire )
 head(extract.fire.all.df)
 
 ## >> calc area per endemism and fire severity ####
 
 endemism.burnt <- extract.fire.all.df %>% 
   mutate(area.adj = area*coverage_fraction) %>%
-  group_by(Endemism, value) %>%
+  group_by(corrected_endemism_quantile, value) %>%
   replace_na(list(value = 0)) %>%
   summarise(burnt.area.km = sum(area.adj)/1000000) %>%
+  # commented out for change in fire severity classes
+  # mutate(fire.severity = factor(recode(value, 
+  #                               '1' = "No data",
+  #                               '2' = "Unburnt",
+  #                               '3' = "Low and moderate",
+  #                               '4' = "High",
+  #                               '5' = "Very high",
+  #                               '0' = "Unburnt"),
+  #                               levels = c("No data",
+  #                                          "Unburnt",
+  #                                          "Low and moderate",
+  #                                          "High",
+  #                                          "Very high"))) %>%
   mutate(fire.severity = factor(recode(value, 
-                                '1' = "No data",
-                                '2' = "Unburnt",
-                                '3' = "Low and moderate",
-                                '4' = "High",
-                                '5' = "Very high",
-                                '0' = "Unburnt"),
-                                levels = c("No data",
-                                           "Unburnt",
-                                           "Low and moderate",
-                                           "High",
-                                           "Very high"))) %>%
-  group_by(Endemism, fire.severity) %>%
-  summarise(burnt.area.km = sum(burnt.area.km)) 
+                                     '1' = "No data",
+                                     '2' = "Unburnt",
+                                     '3' = "Low and moderate",
+                                     '4' = "High and very high",
+                                     '5' = "High and very high",
+                                     '0' = "Unburnt"),
+                              levels = c("No data",
+                                         "Unburnt",
+                                         "Low and moderate",
+                                         "High and very high"))) %>%
+  group_by(corrected_endemism_quantile, fire.severity) %>%
+  summarise(burnt.area.km = sum(burnt.area.km)) %>%
+  group_by(corrected_endemism_quantile) %>% mutate(percent = burnt.area.km/sum(burnt.area.km)*100)
                         
 # plotting overall results  
-ggplot(endemism.burnt, aes(factor(Endemism), burnt.area.km, fill = fire.severity )) +
-  geom_col(position = "dodge" ) +
-  scale_x_discrete(labels=c("1" = "Q1", "2" = "Q2",
-                            "3" = "Q3", "4"="Q4"))
+ggplot(endemism.burnt, aes(factor(corrected_endemism_quantile), burnt.area.km, fill = fire.severity )) +
+  geom_col(position = "dodge" ) 
 
 # Plotting burnt areas only
 
 burnt.endemism.area.plot <- endemism.burnt %>%
   filter(!fire.severity %in% c("Unburnt","No data")) %>%
-  ggplot(aes(factor(Endemism), burnt.area.km, fill = fire.severity )) +
+  ggplot(aes(factor(corrected_endemism_quantile), burnt.area.km, fill = fire.severity )) +
   geom_col(position = "dodge" , color = "black") +
   scale_fill_brewer(palette = "OrRd", "Fire severity")+
-  scale_x_discrete(name = "Endemism quantile", labels=c("1" = "Q1", "2" = "Q2",
-                            "3" = "Q3", "4"="Q4")) +
+  scale_x_discrete(name = "Endemism percentile") +
   scale_y_continuous(expand = c(0, 0)) +
   ylab(expression("Area burnt "~(km^2)))+
   theme_classic()
 
-# cowplot::save_plot("output/endemism_area_0-5-plot.png", burnt.endemism.area.plot, base_height = 6, base_width = 8)
+
+# cowplot::save_plot("output/endemism_area_recl-0-5-plot.png", burnt.endemism.area.plot, base_height = 6, base_width = 8)
 
 ## Map of inputs ####
 
@@ -158,43 +158,54 @@ input.map <- tm_shape(aust.utm) +
               alpha = 0,
               legend.show = F,
               lwd = 2, border.col = 1)+
-tm_shape(m1.utm.crop) +
+ tm_shape(m1.utm.crop) +
    tm_polygons("corrected_endemism_quantile", 
                #style="quantile", 
-               title="Quantile: Corrected weighted endemism",
+               title="Percentile: Corrected weighted endemism",
                lwd = 0.5,
                palette="YlGnBu",
                alpha = 0.5,
                style = "cat")+
-  
  tm_shape(aust.utm) +
   tm_polygons("PLACENAME", 
               alpha = 0,
               legend.show = F,
               lwd = 2, border.col = 1)+
-  
-   tm_shape(analysis.area.utm)+
+  tm_shape(analysis.area.utm)+
   tm_polygons( "Id",
               alpha = 0,
               legend.show = T,
               lwd = 1, border.col = 4,
               title = "Preliminary analysis area",
               label = "")+
-  tm_shape(fire.sev.crop)+
+  tm_shape(fire.sev.crop.recl)+
     tm_raster("severity5_eqar250_native_paa",
               title = "Fire severity",
               style = "cat",
                labels = c("No data",
                           "Unburnt",
                           "Low and moderate",
-                          "High",
-                          "Very high"))
+                          "High and very high"))
    
   
 #tmap_arrange(Corrected.weighted.endemism, input.map, ncol=2 )
 
-# tmap_save(input.map, filename = "output/tmap_inputMap_0-5.png")
+# tmap_save(input.map, filename = "output/tmap_inputMap_recl-0-5.png")
 
 
+## << Moran I test for spatial autocorr in endemism
 
+library(spdep)
+
+# extract the center of each polygon
+coo <- coordinates(m1)
+
+# Search radius to include all neighboring polygon (0 - 200km)
+S.dist  <-  dnearneigh(coo, 0, 200000)  
+
+#identify all neighboring polygons for each polygon in the dataset.
+lw <- nb2listw(S.dist, style="W",zero.policy=T) 
+
+# Run the MC simulation
+MI  <-  moran.mc(m1$corrected_endemism, lw, nsim=9999,zero.policy=T)
 
